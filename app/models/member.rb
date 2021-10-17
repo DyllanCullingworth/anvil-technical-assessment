13a2d6f22e5cc146f7b5a4600e113b4972b961a1
mmdyllan@gmail.com <dyllan@sapphiregemtech.com>
2021-10-17 09:17:08 +0200

    add validations to member.rb to pass tests

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,3 +1,8 @@
 class Member < ApplicationRecord
-
+  validates :name, presence: true
+  validates :surname, presence: true
+  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i }
+  validates :birthday, presence: true
+  validates :games_played, presence: true
+  validates :current_rank, presence: true
 end
